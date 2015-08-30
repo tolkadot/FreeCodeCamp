423e782a10c7895d61c4f770af457ee2f0d28a83
Dee Bryant <tolkadot@gmail.com>
2015-08-30 15:08:56 +1000

    Update javascript.js

--- localWeatherApp/javascript.js
+++ localWeatherApp/javascript.js
@@ -1,7 +1,11 @@
 
+
+var lat
+var long 
+
 function success(pos) {  
-  var long = Math.round(pos.coords.longitude);
-  var lat = Math.round(pos.coords.latitude);
+   long = Math.round(pos.coords.longitude);
+   lat = Math.round(pos.coords.latitude);
 
 }
 
@@ -9,16 +13,20 @@ function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 }
 
-navigator.geolocation.getCurrentPosition(success, error);
+navigator.geolocation.getCurrentPosition(success, error); //get browser location & send the results to success() or error()
 
 
 var req = new XMLHttpRequest() ; //
 
-req.open("GET" , "http://api.openweathermap.org/data/2.5/weather?lat=37&lon=144", false );
+req.open("GET" , "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lat , false );
 req.send();
 
 console.log(req.status);
 console.log(req.statusText);
 
 var responseArr = JSON.parse(req.responseText);
-var tempKelvins = responseArr.main.temp;
+var tempKelvins = Math.round(responseArr.main.temp);
+var tempCelsius = Math.round(tempKelvins- 273.15);
+var tempFarenheit =  Math.round(((tempKelvins - 273)* 1.8 ) + 32);
+
+console.log(tempKelvins,tempCelsius, tempFarenheit );
