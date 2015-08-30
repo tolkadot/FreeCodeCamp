2c3942dd8f6405911d51c6f905b0ba425422a0e5
Dee Bryant <tolkadot@gmail.com>
2015-08-30 18:22:37 +1000

    Update javascript.js

--- localWeatherApp/javascript.js
+++ localWeatherApp/javascript.js
@@ -1,13 +1,7 @@
-
-
-var lat
-var long 
-
-function success(pos) {  
-   long = Math.round(pos.coords.longitude);
-   lat = Math.round(pos.coords.latitude);
-
-}
+var lat //declare  global variables to be used in API URL
+var lon
+ 
+function getCoords(){
 
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
@@ -16,17 +10,29 @@ function error(err) {
 navigator.geolocation.getCurrentPosition(success, error); //get browser location & send the results to success() or error()
 
 
+function success(pos) {  
+   lon = Math.round(pos.coords.longitude);
+   lat = Math.round(pos.coords.latitude);
+   
 var req = new XMLHttpRequest() ; //
-
-req.open("GET" , "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lat , false );
-req.send();
-
+req.open("GET" , "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + " &lon=" + lon, false); //format the API request URL
+req.send(); //send API request
+console.log(lon, lat);
 console.log(req.status);
 console.log(req.statusText);
-
-var responseArr = JSON.parse(req.responseText);
-var tempKelvins = Math.round(responseArr.main.temp);
+console.log(req.responseText);
+var responseArr = JSON.parse(req.responseText); //parse the response
+console.log(responseArr);
+var tempKelvins = Math.round(responseArr.main.temp); //get the temperature data.
 var tempCelsius = Math.round(tempKelvins- 273.15);
-var tempFarenheit =  Math.round(((tempKelvins - 273)* 1.8 ) + 32);
+var tempFarenheit =  Math.round(((tempKelvins - 273) * 1.8 ) + 32);
 
 console.log(tempKelvins,tempCelsius, tempFarenheit );
+
+
+}
+}
+
+getCoords();
+var req = new XMLHttpRequest(); //
+
