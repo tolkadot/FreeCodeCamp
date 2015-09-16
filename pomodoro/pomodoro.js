e881b0b381f45af3e0609653ce7ed3475bb15f5f
Dee Bryant <tolkadot@gmail.com>
2015-09-16 12:58:39 +1000

    Update pomodoro.js

--- pomodoro/pomodoro.js
+++ pomodoro/pomodoro.js
@@ -1,26 +1,46 @@
 var seconds = 0;
 var minutes = 0;
+var dispMin = 0;
 
 var timer = function(){
 
-for (counter = 0; counter < 1200; counter ++)
+for (counter = 1; counter < 180; counter ++)
 {
-    if(counter%60 === 0)
+ if(counter<= 59){
+     minutes = 0;
+     dispMin = minutes + '0';
+     if(counter < 10) {seconds = '0' + counter;}
+     else {seconds = counter;}
+ }   
+ 
+    else if(counter%60 === 0)
     {
-        minutes = minutes+1;
+        minutes += 1;
         seconds = 0+'0';
     }
+    
     else {
-minutes = Math.ceil(counter/60);
-seconds = counter%60;
-if(seconds < 10) {
-    seconds = '0' + seconds;
-}
-}
-console.log(minutes + ":" + seconds)
+       
+    minutes = Math.floor(counter/60);
+    dispMin = minutes;
+     console.log("---");
+    seconds = counter%60;
+    if(seconds < 10) {seconds = '0' + seconds;}
+     }
+     
+if(minutes < 10) { dispMin = '0' + minutes;}
+
+console.log(dispMin + ":" + seconds);
+//updateDisplay();
 }
 
-}
-
+};
 
 timer();
+
+//var updateDisplay = function() {
+//var timerId = setTimeout(timer, 1000);
+//};
+
+
+
